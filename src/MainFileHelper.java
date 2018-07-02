768ae620310c5f71c4f9a1ca2abca394ce1a112b
atnimak <atnimak@gmail.com>
2018-07-02 15:21:01 +0300

    initial commit

---
+++ src/MainFileHelper.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainFileHelper {
+
+    public static void main(String... args) throws IOException {
+        Runtime rt = Runtime.getRuntime();
+        String[] commands = {"exiftool","-keywords","C:\\Users\\Maxim\\Desktop\\14_0503_internet_ban_1.jpg"};
+        Process proc = rt.exec(commands);
+
+        BufferedReader stdInput = new BufferedReader(new
+                InputStreamReader(proc.getInputStream()));
+
+        BufferedReader stdError = new BufferedReader(new
+                InputStreamReader(proc.getErrorStream()));
+
+// read the output from the command
+        System.out.println("Here is the standard output of the command:\n");
+        String s = null;
+        while ((s = stdInput.readLine()) != null) {
+            System.out.println(s);
+        }
+
+// read any errors from the attempted command
+        System.out.println("Here is the standard error of the command (if any):\n");
+        while ((s = stdError.readLine()) != null) {
+            System.out.println(s);
+        }
+    }
+
+
+}
