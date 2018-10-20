f7104f6aee00166178f8ff2d59a75f18e4f16d5c
atnimak <atnimak@gmail.com>
2018-10-20 19:47:36 +0300

    made interacting with the program easier, changed YES to Y(y), NO and EXIT to N(n), also added a wish for a good day at the end

--- src/MainFileHelper.java
+++ src/MainFileHelper.java
@@ -19,12 +19,14 @@ public class MainFileHelper {
         boolean check = true;
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String tmp = "";
-        System.out.println("Привет, это приложение File Helper ver. 0.017 скопирует файлы jpg с ключами и их eps-пары в одну или несколько директорий.\n" +
+        System.out.println("Привет, это приложение File Helper ver. 0.019 скопирует файлы jpg с ключами и их eps-пары в одну или несколько директорий.\n" +
                 "Если в целевых директориях уже есть файлы с именами, совпадающими с копируемыми файлами, эти файлы будут просто перезаписаны.\n" +
                 "Никаких уведомлений в этом случае показано не будет.\n" +
                 "При необходимости исходные файлы могут быть удалены.\n" +
                 "Из-за особенностей коммандной строки Windows программа не умеет работать с кирилицей,\n" +
-                "обратите внимание, чтобы все символы в имени файла и адресе были цифрами или символами латинского алфавита\n");
+                "обратите внимание, чтобы все символы в имени файла и адресе были цифрами или символами латинского алфавита.\n"+
+                "Отвечать на вопросы программы можно используя клвиши Y в значении ДА и N в значении НЕТ,\n" +
+                "также допустимо использование строчных символов y и n.\n");
         do {
             targetDirList = new ArrayList<>();
             filesToCopy = new ArrayList<>();
@@ -49,23 +51,23 @@ public class MainFileHelper {
             check = true;
             do {
                 tmp = "";
-                System.out.println("Введите директорию, в которую необходимо скопировать файлы или NO");
+                System.out.println("Введите директорию, в которую необходимо скопировать файлы или N");
                 tmp = reader.readLine();
                 File targetDir = new File(tmp);
-                if ((!tmp.isEmpty()) && (!(tmp.equals("NO"))) && targetDir.isDirectory()) {
+                if ((!tmp.isEmpty()) && (!(tmp.equals("N")||tmp.equals("n"))) && targetDir.isDirectory()) {
                     targetDirList.add(targetDir);
                     System.out.println("Целевая дирктория принята!");
                 } else if (tmp.isEmpty()) {
                     System.out.println("Адрес директории не может быть пустым");
-                } else if ((tmp.equals("NO")) && targetDirList.size() < 1) {
+                } else if ((tmp.equals("N")||tmp.equals("n")) && targetDirList.size() < 1) {
                     System.out.println("Должна быть хотя бы одна целевая директория");
-                } else if ((tmp.equals("NO")) && targetDirList.size() > 0) {
+                } else if ((tmp.equals("N")||tmp.equals("n")) && targetDirList.size() > 0) {
                     System.out.println("Файлы из директории " + sourceDir.getAbsolutePath() + " будут скопированы в директории: ");
                     for (File s : targetDirList) {
                         System.out.println(s.getAbsolutePath());
                     }
                     check = false;
-                } else if ((!tmp.isEmpty()) && (!(tmp.equals("NO"))) && (!targetDir.isDirectory())) {
+                } else if ((!tmp.isEmpty()) && (!(tmp.equals("N")||tmp.equals("n"))) && (!targetDir.isDirectory())) {
                     System.out.println(targetDir.getAbsolutePath() + " - это не директория!");
                 } else {
                     System.out.println("Что-то пошло не так. Проверьте путь к директории, существует ли директория\n" +
@@ -76,18 +78,18 @@ public class MainFileHelper {
             check = true;
             do {
                 tmp = "";
-                System.out.println("Удалить файлы из исходной аудитории после копирования? YES/NO");
+                System.out.println("Удалить файлы из исходной аудитории после копирования? Y/N");
                 tmp = reader.readLine();
-                if (tmp.equals("YES")) {
+                if (tmp.equals("Y")||tmp.equals("y")) {
                     del = true;
                     System.out.println("Файлы из исходной директории будут удалены!");
                     check = false;
-                } else if (tmp.equals("NO")) {
+                } else if (tmp.equals("N")||tmp.equals("n")) {
                     del = false;
                     System.out.println("Файлы из исходной директории удалены не будут");
                     check = false;
                 } else {
-                    System.out.println("Ответ не принят. Просто введите один из вариантов YES/NO");
+                    System.out.println("Ответ не принят. Просто введите один из вариантов Y или N");
                 }
             } while (check);
             System.out.println("Проводятся запрошенные операции...");
@@ -98,16 +100,17 @@ public class MainFileHelper {
             check = true;
             do {
                 tmp = "";
-                System.out.println("Хотите еще что-нибудь сделать? YES/EXIT");
+                System.out.println("Хотите еще что-нибудь сделать? Y/N");
                 tmp = reader.readLine();
-                if (tmp.equals("YES")) {
+                if (tmp.equals("Y")||tmp.equals("y")) {
                     running = true;
                     check = false;
-                } else if (tmp.equals("EXIT")) {
+                } else if (tmp.equals("N")||tmp.equals("n")) {
+                    System.out.println("Отлично поработали! Хорошего дня!");
                     running = false;
                     check = false;
                 } else {
-                    System.out.println("Ответ не принят. Просто введите один из вариантов YES/EXIT");
+                    System.out.println("Ответ не принят. Просто введите один из вариантов Y/N");
                 }
 
             } while (check);
